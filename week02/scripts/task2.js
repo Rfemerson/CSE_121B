9028c3c10720c08390d95cea74cd68b259ee00ee
Rfemerson <emersonferreirar8@gmail.com>
2023-01-12 20:59:10 -0300

    Finishe Week 2

---
+++ week02/scripts/task2.js
@@ -0,0 +1,71 @@
+/* Lesson 2 */
+
+/* VARIABLES */
+
+// Step 1: declare and instantiate a variable to hold your name
+
+let name = "Emerson Ferreira";
+
+// Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
+
+document.querySelector("#name").innerHTML = name ;
+
+// Step 3: declare and instantiate a variable to hold the current year
+
+let currentYear = "2023";
+
+// Step 4: place the value of the current year variable into the HTML file
+
+document.querySelector("#year").innerHTML = currentYear;
+
+// Step 5: declare and instantiate a variable to hold the name of your picture
+
+const picture = "images/profile.jpg";
+
+// Step 6: copy your image into the "images" folder
+
+
+// Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
+
+document.querySelector("img").setAttribute("src", picture)
+
+/* ARRAYS */
+
+// Step 1: declare and instantiate an array variable to hold your favorite foods
+
+let favoriteFoods = ["Hamburguer", "Pizza", "Pasta", "Ice cream"]
+
+// Step 2: place the values of the favorite foods variable into the HTML file
+
+document.querySelector("#food").innerHTML = favoriteFoods;
+
+// Step 3: declare and instantiate a variable to hold another favorite food
+
+let morefoods = "Barbecue"
+
+// Step 4: add the variable holding another favorite food to the favorite food array
+
+favoriteFoods.push(morefoods);
+
+// Step 5: repeat Step 2
+
+document.querySelector("#food").innerHTML = favoriteFoods;
+
+// Step 6: remove the first element in the favorite foods array
+
+favoriteFoods.shift();
+
+// Step 7: repeat Step 2
+
+document.querySelector("#food").innerHTML = favoriteFoods;
+
+// Step 8: remove the last element in the favorite foods array
+
+favoriteFoods.pop();
+
+// Step 7: repeat Step 2
+
+document.querySelector("#food").innerHTML = favoriteFoods;
+
+
+
